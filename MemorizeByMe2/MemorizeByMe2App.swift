542680bedb157e784e85905f8ebda79c60bb66b0
Leoncio Castillo <leonciocast@hotmail.com>
2021-04-14 10:33:26 -0400

    Initial Commit

---
+++ MemorizeByMe2/MemorizeByMe2App.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeByMe2App.swift
+//  MemorizeByMe2
+//
+//  Created by Leoncio Castillo on 4/14/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeByMe2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
